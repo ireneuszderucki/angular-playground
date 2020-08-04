70b44b7e8b93f51bd527dbfe0ffbc2b46a0ad308
Derucki <diiz@gft.com>
2020-08-04 12:44:40 +0200

    initial commit with custom component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'angular-app';
+  title: string = 'angular-app';
+  title2: string = 'dupa';
 }
